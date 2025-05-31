f3876574cc1b24b1edba1439fb471a5c052867c5
darcy <darcy.ldx@gmail.com>
2025-05-31 09:56:39 -0400

    Fixed the HTML Server Side Issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
+
 import type React from "react"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
+import Head from 'next/head'
 import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
 import Navbar from "@/components/navbar"
@@ -23,7 +25,12 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" className="dark">
+    <html>
+        <Head>
+          {/* Favicon link */}
+          <link rel="icon" href="/logo.png" />
+          {/* You can also add other favicon links or meta tags here */}
+      </Head>
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
           <Navbar />
