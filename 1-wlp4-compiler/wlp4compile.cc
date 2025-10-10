70c9f9342317851ff2e40cf1486c30914c25299e
Darcy Liu <darcy.ldx@gmail.com>
2025-10-10 12:14:32 -0400

    remove the wrapper

--- 1-wlp4-compiler/wlp4compile.cc
+++ 1-wlp4-compiler/wlp4compile.cc
@@ -18,8 +18,7 @@
 #define CS241_SCANNER_H
 
 using namespace std;
-class WLP4Compiler {
-  private:
+
 // Centralized error aggregation for detailed reporting using a CLI string
 struct ErrorAccumulator {
   std::string cliOutput;
@@ -2460,7 +2459,7 @@ string generateCompleteAssembly(string scannedContent) {
   ret += generateFunctionCode(root);
   return ret;
 }
-public:
+
 string wlp4_to_asm(string wlp4_input) {
   resetErrorLog();
   
@@ -2514,7 +2513,7 @@ string wlp4_to_asm(string wlp4_input) {
 
   return assemblyCode;
 }
-};
+
 
 int main() {
   // Read WLP4 input from stdin
